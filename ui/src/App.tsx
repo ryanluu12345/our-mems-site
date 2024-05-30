087a807554d2d10e9e3cd08af49ac8f00f875cba
Ryan Luu <ryanluu12345@gmail.com>
2024-05-30 00:45:38 -0700

    ui: fought through so many system issues and update bugs just to start the UI, just like I fight for our love every day haha

---
+++ ui/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/header";
+import Hero from "./components/hero";
+import BasicTimeline from "./components/timeline";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <BasicTimeline />
+    </div>
+  );
+}
+
+export default App;
